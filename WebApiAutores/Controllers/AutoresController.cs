c7da99abb8a0cd3ea557d7261f4f9f5c57389449
Carlos Daniel <carlosporrasayala@gmail.com>
2024-06-21 02:33:00 -0500

    Actualizar y borrar data a través de endpoints

--- WebApiAutores/Controllers/AutoresController.cs
+++ WebApiAutores/Controllers/AutoresController.cs
@@ -17,10 +17,49 @@ namespace WebApiAutores.Controllers
             return await context.Autores.ToListAsync();
         }
 
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Autor>> Get(int id)
+        {
+            return await context.Autores.FindAsync(id);
+        }
+
         [HttpPost]
         public async Task<ActionResult> Post(Autor autor)
         {
             context.Add(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Put(int id, Autor autor)
+        {
+            if (autor.Id != id)
+            {
+                return BadRequest("El id del autor no coincide con el id de la URL");
+            }
+
+            var existe = await context.Autores.AnyAsync(x => x.Id == id);
+            if (!existe)
+            {
+                return NotFound();
+            }
+
+            context.Update(autor);
+            await context.SaveChangesAsync();
+            return Ok(autor);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var existe = await context.Autores.AnyAsync(x => x.Id == id);
+            if (!existe)
+            {
+                return NotFound();
+            }
+
+            context.Remove(new Autor { Id = id });
             await context.SaveChangesAsync();
             return Ok();
         }
