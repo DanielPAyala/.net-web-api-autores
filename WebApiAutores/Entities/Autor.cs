b49d25ef8adfe4c01b12653428dfd9574011039f
Carlos Daniel <carlosporrasayala@gmail.com>
2024-06-25 12:57:48 -0500

    Crear validación por modelo

--- WebApiAutores/Entities/Autor.cs
+++ WebApiAutores/Entities/Autor.cs
@@ -3,14 +3,28 @@ using WebApiAutores.Validations;
 
 namespace WebApiAutores.Entities
 {
-    public class Autor
+    public class Autor : IValidatableObject
     {
         public int Id { get; set; }
         [Required(ErrorMessage = "El campo {0} es requerido")]
         [StringLength(maximumLength:4, ErrorMessage = "El campo {0} no debe tener más de {1} carácteres")]
-        [PrimeraLetraMayuscula]
+        // [PrimeraLetraMayuscula]
         public string Nombre { get; set; }
 
         public List<Libro> Libros { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!string.IsNullOrEmpty(Nombre))
+            {
+                var primeraLetra = Nombre[0].ToString();
+                if (primeraLetra != primeraLetra.ToUpper())
+                {
+                    yield return new ValidationResult("La primera letra debe ser mayúscula", 
+                        new string[] { nameof(Nombre) });
+                }
+            }
+            
+        }
     }
 }
